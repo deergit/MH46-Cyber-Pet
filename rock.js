8d38cac4f3736a51285babeb8198d8cb042afe51
joeburnsJB <126894305+joeburnsJB@users.noreply.github.com>
2023-03-22 15:34:40 +0000

    Update rock.js

--- rock.js
+++ rock.js
@@ -2,5 +2,21 @@
 import "./pet.js";
 
 class Rock extends Pet {
-    
+    constructor() {
+        this.moss = 0;
+    }
+    play() {
+        if (this.happiness < 80) {
+            this.happiness += 20;
+        }
+        else {
+            this.happiness = 100;
+        }
+        this.moss += 10;
+        // can put line here to change text in the HTML saying that the pet was cleaned
+        return this;
+    }
+    polish() {
+        this.moss = 0;
+    }
 }
