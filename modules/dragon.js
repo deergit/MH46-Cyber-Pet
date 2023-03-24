c4ca73f9736691dbc3d3d8d4dc2e7bf8b3754c9a
joeburnsJB <126894305+joeburnsJB@users.noreply.github.com>
2023-03-24 11:23:08 +0000

    sleep image update dragon

--- modules/dragon.js
+++ modules/dragon.js
@@ -2,6 +2,8 @@
 import Pet from "./pet.js";
 
 export default class Dragon extends Pet {
+    wakeImg = "../images/dragon.png"
+    sleepImg = "../images/dragon_sleep.png"
     greed = 0;
     anger = 0;
     constructor(name) {
